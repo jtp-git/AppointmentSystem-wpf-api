7ab0deeee3a5b14d907819ffcb93ace0b8230a4a
Ja Pastorpide <jtpastorpide@gmail.com>
2026-03-05 23:34:28 +0800

    feat: add Usercontrol for the Data Grid

--- AppointmentSystem.App/App.xaml.cs
+++ AppointmentSystem.App/App.xaml.cs
@@ -7,8 +7,9 @@ namespace AppointmentSystem.App
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+
     }
 
 }

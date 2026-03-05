ca778e72d5d0642bdee080b6930457fc166d8c56
Ja Pastorpide <jtpastorpide@gmail.com>
2026-03-05 22:15:47 +0800

    feat(infrastructure): implement EF Core SQLLite and add project reference to Domain.

---
+++ AppointmentSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppointmentSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Appointment> Appointments { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ AppointmentSystem.Infrastructure/Repositories/AppointmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using AppointmentSystem.Application.Interfaces;
+using AppointmentSystem.Domain.Entities;
+using AppointmentSystem.Infrastructure.Data;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Infrastructure.Repositories
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AppointmentRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(Appointment appointment, CancellationToken cancellationToken)
+        {
+            _appDbContext.Appointments.Add(appointment);
+            await _appDbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

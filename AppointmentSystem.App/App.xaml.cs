a9c0c4fbd4dc3b7466343c45b069bd021d0f19a1
Ja Pastorpide <jtpastorpide@gmail.com>
2026-03-05 23:36:23 +0800

    chore(wpf): dependency injection

--- AppointmentSystem.App/App.xaml.cs
+++ AppointmentSystem.App/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using AppointmentSystem.App.Services;
+using AppointmentSystem.App.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
+using System.Net.Http;
 using System.Windows;
 
 namespace AppointmentSystem.App
@@ -9,7 +13,24 @@ namespace AppointmentSystem.App
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        public static IServiceProvider Services { get; private set; }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7106/")
+            });
+
+            services.AddSingleton<AppointmentApiService>();
+            services.AddSingleton<AppointmentListViewModel>();
+
+            Services = services.BuildServiceProvider();
+
+            base.OnStartup(e);
+        }
     }
 
 }

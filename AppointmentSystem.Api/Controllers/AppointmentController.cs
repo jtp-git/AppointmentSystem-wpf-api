05f29e77fdd95834ed6cf2fdd2fefa389a59d2b0
Ja Pastorpide <jtpastorpide@gmail.com>
2026-03-05 22:32:29 +0800

    feat(api): add appointment create REST endpoint

---
+++ AppointmentSystem.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using AppointmentSystem.Application.DTO;
+using AppointmentSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("appointments")]
+    public class AppointmentController : Controller
+    {
+        private readonly IAppointmentService _appointmentService;
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAppointment(AppointmentDto dto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var createdAppointment = await _appointmentService.CreateAsync(dto, cancellationToken);
+                return CreatedAtAction(nameof(CreateAppointment),
+                    new { id = createdAppointment.Id },
+                    createdAppointment);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}

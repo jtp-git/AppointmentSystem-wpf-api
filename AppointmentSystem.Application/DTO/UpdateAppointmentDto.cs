7b85cdf3d6d0a0ed1c7c60c0000375f8b73a546d
Ja Pastorpide <jtpastorpide@gmail.com>
2026-03-11 22:14:48 +0800

    fix(application): refactor Update DTO, removed private set for ID

--- AppointmentSystem.Application/DTO/UpdateAppointmentDto.cs
+++ AppointmentSystem.Application/DTO/UpdateAppointmentDto.cs
@@ -8,7 +8,7 @@ namespace AppointmentSystem.Application.DTO
 {
     public class UpdateAppointmentDto
     {
-        public int Id { get; private set; }
+        public int Id { get;  set; }
         public string Patient { get;  set; } = null!;
         public DateTime StartTime { get;  set; }
         public DateTime EndTime { get;  set; }

a6d35a4078f40704e38fc9a3813405f12390075a
Ja Pastorpide <jtpastorpide@gmail.com>
2026-03-11 23:32:01 +0800

    feature(app): CRUD on WPF

--- AppointmentSystem.App/App.xaml.cs
+++ AppointmentSystem.App/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AppointmentSystem.App.Services;
 using AppointmentSystem.App.ViewModels;
+using AppointmentSystem.Application.DTO;
 using Microsoft.Extensions.DependencyInjection;
 using System.Configuration;
 using System.Data;
@@ -16,7 +17,7 @@ namespace AppointmentSystem.App
     {
         public static IServiceProvider Services { get; private set; }
 
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
             var services = new ServiceCollection();
 
@@ -31,6 +32,48 @@ namespace AppointmentSystem.App
             Services = services.BuildServiceProvider();
 
             base.OnStartup(e);
+
+
+            // TEMPORARY TEST
+            //var api = Services.GetRequiredService<IAppointmentApiService>();
+
+
+            //var appointment = await api.GetAppointmentByIdAsync(1, CancellationToken.None);
+
+            //if (appointment != null)
+            //{
+            //    MessageBox.Show($"Appointment: {appointment.Patient}");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Appointment not found");
+            //}
+
+
+
+            //var dto = new CreateAppointmentDto
+            //{
+            //    Patient = "Create Test",
+            //    StartTime = DateTime.Now.AddHours(1),
+            //    EndTime = DateTime.Now.AddHours(2),
+            //    Notes = "Initial consultation"
+            //};
+
+            //await api.CreateAsync(dto, CancellationToken.None);
+
+            //MessageBox.Show("Appointment created successfully!");
+
+
+            //var appointment = await api.GetAppointmentByIdAsync(1, CancellationToken.None);
+
+            //if (appointment != null)
+            //{
+            //    MessageBox.Show($"Appointment Found: {appointment.Patient}");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Appointment not found");
+            //}
         }
     }
 
